fd57bc39a703d63dd8fbe876e7a419e15025cd36
raaj <rajmunkhan963@gmail.com>
2022-05-03 21:47:21 +0530

    created the connection

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
 
-$con = mysqli_connect("localhost","root","","developers_test");
+$con = mysqli_connect("localhost:3307","root","","developers_test");
 	if (mysqli_connect_errno())
 	{
 		echo "Failed to connect to MySQL: " . mysqli_connect_error();
